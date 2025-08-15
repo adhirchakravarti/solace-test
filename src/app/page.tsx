155037400ce4a6cb50347c26cff507769dc9ec7c
Adhir Chakravarti <adhir.chakravarti@gmail.com>
2025-08-14 17:39:34 -0700

    fix: bugs and errors on the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,50 @@
 "use client";
 
+import { SelectAdvocates } from "@/db/schema";
+import { typedFetch } from "@/utils/typedFetch";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [searchTerm, setSearchTerm] = useState<string>();
+  const [advocates, setAdvocates] = useState<SelectAdvocates[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<SelectAdvocates[]>(
+    []
+  );
 
   useEffect(() => {
     console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
+    async function getAdvocates() {
+      try {
+        const advocateData = await typedFetch<{ data: SelectAdvocates[] }>(
+          "/api/advocates"
+        );
+        console.log({ advocateData });
+        if (advocateData?.data && advocateData.data.length) {
+          setAdvocates(advocateData.data);
+          setFilteredAdvocates(advocateData.data);
+        }
+      } catch (error) {
+        console.warn('Error when fetching files', error)
+      }
+    }
+    getAdvocates();
   }, []);
 
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.trim()?.toLowerCase();
+    setSearchTerm(searchTerm);
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
       return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        advocate.firstName.toLowerCase().includes(searchTerm) ||
+        advocate.lastName.toLowerCase().includes(searchTerm) ||
+        advocate.city.toLowerCase().includes(searchTerm) ||
+        advocate.degree.toLowerCase().includes(searchTerm) ||
+        advocate.specialties.some((s) =>
+          s.toLowerCase().includes(searchTerm)
+        ) ||
+        advocate.yearsOfExperience.toString().includes(searchTerm)
       );
     });
 
@@ -49,7 +64,7 @@ export default function Home() {
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for: <span id="search-term">{searchTerm}</span>
         </p>
         <input style={{ border: "1px solid black" }} onChange={onChange} />
         <button onClick={onClick}>Reset Search</button>
@@ -58,25 +73,27 @@ export default function Home() {
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.id}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
                   {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                    <div key={s}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>

b4a79dbd799a5d2c0879988e6a2289ed4843f608
Adhir Chakravarti <adhir.chakravarti@gmail.com>
2025-08-14 23:30:00 -0700

    chore: install dotenv and slightly adjust the drizzle config and migration files

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,15 +1,16 @@
 const { drizzle } = require("drizzle-orm/postgres-js");
 const { migrate } = require("drizzle-orm/postgres-js/migrator");
 const postgres = require("postgres");
+require("dotenv").config();
 
 const runMigration = async () => {
   if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
   console.log(process.env.DATABASE_URL);
 
-  const sql = postgres(process.env.DATABASE_URL, { max: 1 });
+  const sql = postgres(process.env.DATABASE_URL, { max: 1, database: "solaceassignment" });
   const db = drizzle(sql);
-  await migrate(db, { migrationsFolder: "drizzle" });
+  await migrate(db, { migrationsFolder: "src/db/migrations" });
   await sql.end();
 };
 
